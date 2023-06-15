ed9cef660801e5c729fecf02bae060104b9420ce
Franklin Hau <fhauchi@gmail.com>
2023-06-15 15:27:29 -0600

    lab finish

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import './NavBar.css';
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className='navbar'>
+      <NavLink className='nav-link' to='/'>Home </NavLink>
+      <NavLink className='nav-link' to='/movies'>Movies </NavLink>
+      <NavLink className='nav-link' to='/directors'>Directors </NavLink>
+      <NavLink className='nav-link' to='/actors'>Actors </NavLink>
+    </div>
+    );
 }
 
 export default NavBar;
