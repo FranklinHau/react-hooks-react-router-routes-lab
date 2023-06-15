b8b1257ab77ab0a42cce7b374443c2c113b693e8
Franklin Hau <fhauchi@gmail.com>
2023-06-15 15:35:24 -0600

    delete css file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import './NavBar.css';
+
 
 function NavBar() {
   return (
     <div className='navbar'>
-      <NavLink className='nav-link' to='/'>Home </NavLink>
-      <NavLink className='nav-link' to='/movies'>Movies </NavLink>
-      <NavLink className='nav-link' to='/directors'>Directors </NavLink>
-      <NavLink className='nav-link' to='/actors'>Actors </NavLink>
+      <NavLink to='/'>Home </NavLink>
+      <NavLink to='/movies'>Movies </NavLink>
+      <NavLink to='/directors'>Directors </NavLink>
+      <NavLink to='/actors'>Actors </NavLink>
     </div>
     );
 }
